624afe0c7f7ecbf23bd0ae83e482600ddf038ab6
Daniel Truong <ddtruong06@gmail.com>
2022-03-14 22:12:12 -0700

    Cors pls

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
